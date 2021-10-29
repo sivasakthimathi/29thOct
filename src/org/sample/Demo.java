99e9dc7ca15543a5aa50d0d27e660dd6c1bc44db
sivasakthimathi <lvpsivasakthi2@gmail.com>
2021-10-29 10:22:07 +0530

    newly created

---
+++ src/org/sample/Demo.java
@@ -0,0 +1,21 @@
+package org.sample;
+
+public class Demo {
+  private static int empid( int a,int b) {
+	  System.out.println(a+b);
+	  return a+b;
+
+}public static void main(String[] args) {
+	Demo d = new Demo();
+	d.empid(10, 20);
+	System.out.println(empid(10, 10)/2);
+//	System.out.println(emp*2);
+	
+	
+	
+}
+	
+	
+	
+	
+}
